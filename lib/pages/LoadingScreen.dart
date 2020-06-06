d3a55e05324583d4158f26f4b06295808fdd7ea4
telentovj <johnvijaybalasupramaniam@gmail.com>
2020-06-06 23:24:07 +0800

    login

--- lib/pages/LoadingScreen.dart
+++ lib/pages/LoadingScreen.dart
@@ -1,56 +1,18 @@
-import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:recipemine/pages/HomePage.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
-class LoadingScreen extends StatefulWidget {
-  @override
-  _LoadingScreenState createState() => _LoadingScreenState();
-}
-
-class _LoadingScreenState extends State<LoadingScreen> {
+//Class is for loading when accessing menus within the app.
+class Loading extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.fromLTRB(30.0, 0.0, 30.0, 0.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: <Widget>[
-              Image(image: AssetImage("assets/Logo.png")),
-              Padding( // Motto underneath logo
-                padding: const EdgeInsets.fromLTRB(0.0, 5.0, 0.0, 0.0),
-                child: Text(
-                  "Find the perfect recipe",
-                  style: TextStyle(
-                    fontFamily: "SanFranciscoDisplay",
-                    fontSize: 16.0,
-                    fontWeight: FontWeight.w500,
-                    color: Color(0xff5F3739).withOpacity(0.70),
-                  ),
-                ),
-              ),
-            ],
-          ),
+    return Container(
+      color: Colors.white,
+      child: Center(
+        child: SpinKitChasingDots(
+          color: Colors.pinkAccent,
+          size: 50.0,
         ),
       ),
     );
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    transition();
-  }
-
-  // method used to transition to the homescreen.
-  // copied from stackover flow, don't know why this code works.
-  // Better to leave it.
-  void transition() async {
-    Timer(
-        Duration(seconds: 0),
-        () => Navigator.of(context).pushReplacement(
-            MaterialPageRoute(builder: (BuildContext context) => SearchPage())));
   }
 }
