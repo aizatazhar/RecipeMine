5333ae29a375ec3c54c3e1f7587b00689525681e
Aizat Azhar <aizat.azharr@gmail.com>
2020-06-30 17:54:39 +0800

    Renamed Alarm widget, fixed responsiveness issue

---
+++ lib/Custom/CustomWidgets/Alarm.dart
@@ -0,0 +1,115 @@
+import "package:flutter/material.dart";
+import "package:numberpicker/numberpicker.dart";
+import "package:countdown_flutter/countdown_flutter.dart";
+
+class Alarm extends StatefulWidget {
+  @override
+  _AlarmState createState() => _AlarmState();
+}
+
+// Class used to set the count down timing
+class _AlarmState extends State<Alarm> {
+  int hour = 0;
+  int minute = 0;
+  int second = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        SizedBox(height: 50),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Column(
+              children: <Widget>[
+                Text("Hrs"),
+                NumberPicker.integer(
+                  initialValue: 0,
+                  minValue: 0,
+                  maxValue: 24,
+                  infiniteLoop: true,
+                  onChanged: (val) => hour = val,
+                ),
+              ],
+            ),
+            Column(
+              children: <Widget>[
+                Text("Mins"),
+                NumberPicker.integer(
+                  initialValue: 0,
+                  minValue: 0,
+                  maxValue: 60,
+                  infiniteLoop: true,
+                  onChanged: (val) => minute = val,
+                ),
+              ],
+            ),
+            Column(
+              children: <Widget>[
+                Text("Sec"),
+                NumberPicker.integer(
+                  initialValue: 0,
+                  minValue: 0,
+                  maxValue: 60,
+                  infiniteLoop: true,
+                  onChanged: (val) => second = val,
+                ),
+              ],
+            ),
+          ],
+        ), // Row of NumberPickers
+        SizedBox(height: 20),
+        Center( // Start button
+          child: Container(
+            child: Ink(
+              decoration: ShapeDecoration(
+                color: Colors.pink,
+                shape: CircleBorder(),
+              ),
+              child: IconButton(
+                  icon: Icon(Icons.play_arrow),
+                  color: Colors.white,
+                  onPressed: () {
+                    showModalBottomSheet(
+                        context: context,
+                        builder: (context) {
+                          return Center(child: CountDown(hour, minute, second));
+                        });
+                  }),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+// Class used to show the countdown
+class CountDown extends StatefulWidget {
+  final int hour;
+  final int minute;
+  final int second;
+  const CountDown(this.hour, this.minute, this.second);
+  @override
+  _CountDownState createState() => _CountDownState();
+}
+
+class _CountDownState extends State<CountDown> {
+  @override
+  Widget build(BuildContext context) {
+    return CountdownFormatted(
+      duration: Duration(
+        hours: widget.hour,
+        minutes: widget.minute,
+        seconds: widget.second,
+      ),
+      builder: (BuildContext ctx, String remaining) {
+        return Text(
+          remaining,
+          style: TextStyle(fontSize: 35),
+        ); // 01:00:00
+      },
+    );
+  }
+}
